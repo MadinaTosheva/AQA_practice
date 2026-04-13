57ea2a4c22964ec36f5d543c1d1040e9de562ab2
madinabonu.tosheva <madinatoseva4@gmail.com>
2026-04-13 10:07:19 +0500

    Переписала все константы с заглавными буквами

--- demoQA.py
+++ demoQA.py
@@ -3,14 +3,14 @@ from playwright.sync_api import sync_playwright, expect
 
 BASE_URL = "https://demoqa.com"
 
-DOUBLE_CLICK_BUTTON_loc = "#doubleClickBtn"
-CLICK_ME_BUTTON_loc = "div.mt-4:nth-child(4) button"
-DINAMIC_CLICK_loc = '#dynamicClickMessage'
-FULL_NAME_INPUT_loc = "#userName"
-EMAIL_INPUT_loc = '#userEmail'
-CURRENT_ADDRESS_loc = "#currentAddress-label"
+DOUBLE_CLICK_BUTTON_LOC = "#doubleClickBtn"
+CLICK_ME_BUTTON_LOC = "div.mt-4:nth-child(4) button"
+DINAMIC_CLICK_LOC = '#dynamicClickMessage'
+FULL_NAME_INPUT_LOC = "#userName"
+EMAIL_INPUT_LOC = '#userEmail'
+CURRENT_ADDRESS_LOC = "#currentAddress-label"
 
-SELECT_VALUE_loc = "#withOptGroup"
+SELECT_VALUE_LOC = "#withOptGroup"
 
 @pytest.fixture
 def launch_browser():
@@ -28,7 +28,7 @@ def test_task_01(launch_browser):
     page = launch_browser
 
     page.goto(f"{BASE_URL}/buttons")
-    double_click_button = page.locator(DOUBLE_CLICK_BUTTON_loc).inner_text()
+    double_click_button = page.locator(DOUBLE_CLICK_BUTTON_LOC).inner_text()
     assert double_click_button == "Double Click Me"
 
 
@@ -36,9 +36,9 @@ def test_task_02(launch_browser):
     page = launch_browser
 
     page.goto(f"{BASE_URL}/buttons")
-    page.locator(CLICK_ME_BUTTON_loc).click()
-    expect(page.locator(DINAMIC_CLICK_loc)).to_have_text("You have done a dynamic click")
-    dynamic_click_message = page.locator(DINAMIC_CLICK_loc).inner_text()
+    page.locator(CLICK_ME_BUTTON_LOC).click()
+    expect(page.locator(DINAMIC_CLICK_LOC)).to_have_text("You have done a dynamic click")
+    dynamic_click_message = page.locator(DINAMIC_CLICK_LOC).inner_text()
     # expect(dynamic_click_message).to_have_text("You have done a right click")
 
     assert dynamic_click_message == "You have done a dynamic click"
@@ -48,10 +48,10 @@ def test_task_03(launch_browser):
     page = launch_browser
 
     page.goto(f"{BASE_URL}/text-box")
-    user_name_input = page.locator(FULL_NAME_INPUT_loc)
+    user_name_input = page.locator(FULL_NAME_INPUT_LOC)
     user_name_input.fill("Madina")
 
-    user_email_input = page.locator(EMAIL_INPUT_loc)
+    user_email_input = page.locator(EMAIL_INPUT_LOC)
     user_email_input.fill("test@gmail.com")
 
     print(f"Имя: {user_name_input.input_value()},\n"
@@ -62,7 +62,7 @@ def test_task_04(launch_browser):
     page = launch_browser
 
     page.goto(f"{BASE_URL}/select-menu")
-    select_value_dropdown = page.locator(SELECT_VALUE_loc)
+    select_value_dropdown = page.locator(SELECT_VALUE_LOC)
     select_value_dropdown_list = select_value_dropdown.get_by_role("option")
     assert select_value_dropdown_list.count() == 0
     select_value_dropdown.click()
@@ -79,7 +79,7 @@ def test_task_05(launch_browser):
     page = launch_browser
 
     page.goto(f"{BASE_URL}/text-box")
-    current_address_input = page.locator(CURRENT_ADDRESS_loc)
+    current_address_input = page.locator(CURRENT_ADDRESS_LOC)
 
     print(current_address_input.inner_text())
     print(current_address_input.text_content())
--- herokuapp.py
+++ herokuapp.py
@@ -3,11 +3,11 @@ from playwright.sync_api import sync_playwright, expect
 
 BASE_URL = "https://the-internet.herokuapp.com"
 
-HEADER_TITLE_loc = "#content h2"
-USERNAME_loc = "#username"
-PASSWORD_loc = "#password"
-LOGIN_loc = "[type='submit']"
-VALIDATION_ERROR_loc = "#flash"
+HEADER_TITLE_LOC = "#content h2"
+USERNAME_LOC = "#username"
+PASSWORD_LOC = "#password"
+LOGIN_LOC = "[type='submit']"
+VALIDATION_ERROR_LOC = "#flash"
 
 
 @pytest.fixture
@@ -26,7 +26,7 @@ def test_task_06(launch_browser):
     page = launch_browser
 
     page.goto(BASE_URL)
-    header_loc = page.locator(HEADER_TITLE_loc)
+    header_loc = page.locator(HEADER_TITLE_LOC)
     header_text = header_loc.inner_text()
     assert "Available Examples" == header_text
     expect(header_loc).to_contain_text(header_text)
@@ -36,11 +36,11 @@ def test_task_07(launch_browser):
     page = launch_browser
 
     page.goto(f"{BASE_URL}/login")
-    page.locator(USERNAME_loc).fill("tomsmith ")
-    page.locator(PASSWORD_loc).fill("SuperSecretPassword!")
-    page.locator(LOGIN_loc).click()
+    page.locator(USERNAME_LOC).fill("tomsmith ")
+    page.locator(PASSWORD_LOC).fill("SuperSecretPassword!")
+    page.locator(LOGIN_LOC).click()
 
-    validation_error = page.locator(VALIDATION_ERROR_loc)
+    validation_error = page.locator(VALIDATION_ERROR_LOC)
     expect(validation_error).to_be_visible()
 
     error_text = validation_error.inner_text()
